9cbcc77ba05e5b96df9f95ac465dcdb9c1d6600d
Bartosz Cytrowski <bcytrowski@atlassian.com>
2019-08-01 15:42:11 +0200

    Splitting actions into separate files

---
+++ frontend/src/apps/device/store/device.actions.js
@@ -0,0 +1,211 @@
+import cancellationToken from "utils/cancellation-token";
+import screenfull from "screenfull";
+import axios from "axios";
+import ms from "ms";
+
+import * as api from "services/api";
+import { createDevice, getDeviceDetails, removeDevice } from "services/api";
+
+import {
+  currentMeetingSelector,
+  isCalendarSelectedSelector,
+  isDashboardDeviceSelector,
+  isDeviceRemovedSelector,
+  isInitializedSelector,
+  isInOfflineModeSelector,
+  isSubscriptionCancelledSelector,
+  lastActivityOnShowCalendarsViewSelector,
+  minutesLeftForCheckInSelector,
+  showAllCalendarsViewSelector
+} from "./selectors";
+import { changeLanguage } from "i18n";
+
+import { wait, waitUntilTrue } from "utils/time";
+
+import { $updateClock } from "./state/timestamp/timestamp.duck";
+import { $startClock } from "./state/timestamp/timestamp.thunks";
+
+import { $updateDeviceData } from "./state/device/device.duck";
+
+import {
+  endAction
+} from "./state/currentMeetingActions/currentMeetingActions.duck";
+
+import {
+  $markInitialized,
+  $markRemoved,
+  $setIsSubscriptionCancelled,
+  $updateOfflineStatus,
+  $updateShowAllCalendarsView,
+  $allCalendarsViewActivity
+} from "./state/appState/appState.duck";
+
+import { $updateFullScreenState, $changeFontSize } from "./state/displayOptions/displayOptions.duck";
+
+const $initializeApiVersionObserver = () => async () => {
+  let currentVersion = undefined;
+
+  const checkVersion = async () => {
+    const response = await api.getApiVersion();
+
+    if (response && response.version && currentVersion && currentVersion !== response.version) {
+      window.location.reload();
+    }
+
+    if (response) {
+      currentVersion = response.version;
+    }
+  };
+
+  setInterval(checkVersion, 1000 * 60 * 5);
+  await checkVersion();
+};
+
+const $initializeFullScreenSupport = () => dispatch => {
+  const updateStatus = () => {
+    dispatch($updateFullScreenState(screenfull.enabled, screenfull.isFullscreen));
+  };
+
+  updateStatus();
+
+  if (typeof screenfull.onchange === "function") {
+    screenfull.onchange(updateStatus);
+  }
+};
+
+export const deviceActions = {
+  initialize: () => async (dispatch, getState) => {
+    if (isInitializedSelector(getState())) {
+      return;
+    }
+
+    dispatch($initializeApiVersionObserver());
+    dispatch($markInitialized());
+
+    try {
+      await getDeviceDetails();
+    } catch (error) {
+      if (error.response && error.response.status === 403) {
+        await createDevice();
+      }
+    }
+
+    dispatch($startClock());
+    dispatch(deviceActions.$fetchDeviceData());
+    dispatch($initializeFullScreenSupport());
+    dispatch(deviceActions.$initializeOfflineObserver());
+  },
+
+  $fetchDeviceData: () => async (dispatch, getState) => {
+    const token = cancellationToken(deviceActions.$fetchDeviceData).cancelOthers();
+
+    try {
+      const shouldGetAllCalendars = showAllCalendarsViewSelector(getState());
+      const device = await getDeviceDetails(shouldGetAllCalendars);
+
+      if (device.version && device.version !== window.version) {
+        window.location.reload();
+      }
+
+      if (token.isCancelled()) {
+        return;
+      }
+
+      changeLanguage(device.language);
+
+      dispatch($updateDeviceData(device));
+      dispatch($setIsSubscriptionCancelled(false));
+      dispatch(deviceActions.$removeCurrentMeetingIfNotCheckedIn());
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        dispatch($markRemoved());
+      }
+      if (error.response && error.response.status === 402) {
+        dispatch($setIsSubscriptionCancelled(true));
+      }
+    }
+
+    const timeout = (function() {
+      const state = getState();
+
+      if (isDeviceRemovedSelector(state)) return ms("1 year");
+      if (isSubscriptionCancelledSelector(state)) return ms("10 min");
+      if (isDashboardDeviceSelector(state) || isCalendarSelectedSelector(state)) return ms("30s");
+
+      return ms("5s");
+    })();
+
+    await wait(timeout);
+
+    if (token.isCancelled()) {
+      return;
+    }
+
+    dispatch(deviceActions.$fetchDeviceData());
+  },
+  $removeCurrentMeetingIfNotCheckedIn: () => async (dispatch, getState) => {
+    const state = getState();
+    const minutesLeftForCheckIn = minutesLeftForCheckInSelector(state);
+    const meeting = currentMeetingSelector(state);
+
+    if (minutesLeftForCheckIn !== null && minutesLeftForCheckIn < 0) {
+      await api.deleteMeeting(meeting.id, true);
+      dispatch(deviceActions.$fetchDeviceData());
+    }
+  },
+
+  $updateClock,
+
+  toggleFullScreen: () => () => {
+    if (screenfull.enabled) {
+      screenfull.toggle();
+    }
+  },
+
+  // TODO - find all usages of changeFontSize and rename it to $changeFontSize
+  changeFontSize: $changeFontSize,
+
+  $initializeOfflineObserver: () => (dispatch, getState) => {
+    const successCallback = result => {
+      if (isInOfflineModeSelector(getState())) {
+        dispatch(endAction());
+        dispatch($updateOfflineStatus(false));
+      }
+
+      return result;
+    };
+
+    const errorCallback = error => {
+      if (error.response === undefined && !isInOfflineModeSelector(getState())) {
+        dispatch($updateOfflineStatus(true));
+      }
+
+      return Promise.reject(error);
+    };
+
+    axios.interceptors.response.use(successCallback, errorCallback);
+  },
+
+  disconnectDevice: () => async () => {
+    await removeDevice();
+    window.location.reload();
+  },
+
+  $updateShowAllCalendarsView,
+  $allCalendarsViewActivity,
+
+  showAllCalendarsView: () => async (dispatch, getState) => {
+    dispatch(deviceActions.$updateShowAllCalendarsView(true));
+    dispatch(deviceActions.$allCalendarsViewActivity());
+    dispatch(deviceActions.$fetchDeviceData());
+
+    await waitUntilTrue(() => lastActivityOnShowCalendarsViewSelector(getState()) < Date.now() - 30 * 1000);
+
+    dispatch(deviceActions.closeAllCalendarsView());
+  },
+
+  closeAllCalendarsView: () => dispatch => {
+    dispatch(endAction());
+    dispatch(deviceActions.$updateShowAllCalendarsView(false));
+  }
+};
---
+++ frontend/src/apps/device/store/meeting.actions.js
@@ -0,0 +1,115 @@
+import {
+  $setActionSource,
+  $startAction,
+  $setActionIsRetrying,
+  $setActionError,
+  $setActionSuccess,
+  endAction
+} from "./state/currentMeetingActions/currentMeetingActions.duck";
+
+import { $updateDeviceData } from "./state/device/device.duck";
+
+import { calendarNameSelector, currentActionSelector, currentMeetingSelector } from "./selectors";
+
+import * as api from "services/api";
+
+import i18next from "i18next";
+
+import { getDeviceDetails } from "services/api";
+
+const createMeetingInAnotherRoom = (calendarId, timeInMinutes) => async (dispatch, getState) => {
+  dispatch($startAction(createMeetingInAnotherRoom(calendarId, timeInMinutes)));
+
+  const roomName = calendarNameSelector(getState(), { calendarId });
+
+  try {
+    await api.createMeeting(timeInMinutes, i18next.t("meeting.quick-meeting-title", { roomName }), calendarId);
+
+    dispatch($updateDeviceData(await getDeviceDetails(true)));
+    dispatch($setActionSuccess());
+  } catch (error) {
+    console.error(error);
+    dispatch($setActionError());
+  }
+};
+
+const $handleMeetingActionPromise = actionPromise => async dispatch => {
+  try {
+    await actionPromise;
+
+    dispatch($updateDeviceData(await getDeviceDetails()));
+    dispatch(endAction());
+  } catch (error) {
+    console.error(error);
+
+    dispatch($updateDeviceData(await getDeviceDetails()));
+    dispatch($setActionError(error && error.response && error.response.status));
+  }
+};
+
+const $updateCurrentMeeting = options => (dispatch, getState) => {
+  const currentMeetingId = currentMeetingSelector(getState()).id;
+  const updateMeetingPromise = api.updateMeeting(currentMeetingId, options);
+
+  dispatch($handleMeetingActionPromise(updateMeetingPromise));
+};
+
+const startMeetingEarly = () => async dispatch => {
+  dispatch($startAction(startMeetingEarly()));
+
+  dispatch($updateCurrentMeeting({ checkIn: true, startNow: true }));
+};
+
+const endMeeting = () => dispatch => {
+  dispatch($startAction(endMeeting()));
+
+  dispatch($updateCurrentMeeting({ endNow: true }));
+};
+
+const checkInToMeeting = () => dispatch => {
+  dispatch($startAction(checkInToMeeting()));
+
+  dispatch($updateCurrentMeeting({ checkIn: true }));
+};
+
+const extendMeeting = timeInMinutes => async dispatch => {
+  dispatch($startAction(extendMeeting(timeInMinutes)));
+
+  dispatch($updateCurrentMeeting({ extensionTime: timeInMinutes }));
+};
+
+const retry = () => (dispatch, getState) => {
+  dispatch($setActionIsRetrying());
+  dispatch(currentActionSelector(getState()));
+};
+
+const createMeeting = timeInMinutes => (dispatch, getState) => {
+  dispatch($startAction(createMeeting(timeInMinutes)));
+
+  const roomName = calendarNameSelector(getState());
+  const createMeetingPromise = api.createMeeting(timeInMinutes, i18next.t("meeting.quick-meeting-title", { roomName }));
+
+  dispatch($handleMeetingActionPromise(createMeetingPromise));
+};
+
+const cancelMeeting = () => async (dispatch, getState) => {
+  dispatch($startAction(cancelMeeting()));
+
+  const currentMeetingId = currentMeetingSelector(getState()).id;
+  const deleteMeetingPromise = api.deleteMeeting(currentMeetingId, false);
+
+  dispatch($handleMeetingActionPromise(deleteMeetingPromise));
+};
+
+export const meetingActions = {
+  endAction,
+  $setActionSource,
+  retry,
+  createMeeting,
+  cancelMeeting,
+  endMeeting,
+  checkInToMeeting,
+  extendMeeting,
+  startMeetingEarly,
+  createMeetingInAnotherRoom
+};
